b8b5bdcdd88136b2ac2f87e422040819e8eb79a3
Cat Soup <98920372+CatSoup1@users.noreply.github.com>
2022-08-01 20:16:56 +1000

    asassas

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = 3000 || process.env.PORT
+const path = require('path')   
 
 app.get('/', (req, res) => {
-    res.send("hi")
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+app.get('/krispykitchen', (req, res) => {
+    res.send("balls")
 })
 
 app.listen(port, () => {
